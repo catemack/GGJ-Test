2fbde3fbae4342707b47f1caef8933903bfa4ec7
khp <kanghee.p@gmail.com>
2015-01-23 21:14:54 -0800

    Removed badlogic image

--- project/core/src/com/mygdx/game/MyGdxGame.java
+++ project/core/src/com/mygdx/game/MyGdxGame.java
@@ -8,12 +8,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
