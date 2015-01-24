feec7e3c5481540f9e4b7dff4d2cedfa4b5c9a7c
rhint <rswanson5@gmail.com>
2015-01-23 21:31:10 -0800

    added player superclass

---
+++ project/core/src/com/mygdx/game/Player.java
@@ -0,0 +1,29 @@
+package com.mygdx.game;
+
+public class Player {
+	
+	// x and y coordinates of the player sprite
+	private int x;
+	private int y;
+	// speed of player sprite in two axes
+	private int dx;
+	private int dy;
+	
+	public int getX() {
+		// Return the x position of the player
+		return 0;
+	}
+	
+	public int getY() {
+		// Return the y position of the player
+		return 0;
+	}
+	
+	public void move() {
+		// Move the sprite
+	}
+
+	public void jump() {
+		// Sprite jumps
+	}
+}
